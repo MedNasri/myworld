4b1bc9edc4434b51894a3bc4a7f5a1e66c397f7c
MedNasri <62633186+MedNasri@users.noreply.github.com>
2020-09-18 17:49:49 +0200

    With case

--- src/Redux/reducers/LocationReducer.js
+++ src/Redux/reducers/LocationReducer.js
@@ -12,3 +12,22 @@ const fetchLocation = (state = defaultState, action) => {
 };
 
 export default fetchLocation;
+
+// const defaultState = {
+//     location: []
+// };
+
+// const fetchLocation = (state = defaultState, action) => {
+//     switch (action.payload) {
+//         case FETCH_LOCATION : {
+//             return { ...state, location: action.payload };
+//           }
+//         default:
+//             return state;
+//     }
+// };
+
+// export default fetchLocation;
+
+  
+
